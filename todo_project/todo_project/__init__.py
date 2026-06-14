1395a4ddf58a2b2bdc6d48da2e49d78965c69030
Rafael Lima <rafaelima1250@gmail.com>
2026-06-14 19:52:31 -0300

    Adiciona monitoramento com Prometheus e Grafana

--- todo_project/todo_project/__init__.py
+++ todo_project/todo_project/__init__.py
@@ -2,12 +2,32 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-
+from flask import request, Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '45cf93c4d41348cd9980674ade9a7356'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+REQUEST_COUNT = Counter(
+    "flask_http_requests_total",
+    "Total de requisicoes HTTP recebidas pela aplicacao Flask",
+    ["method", "endpoint", "status"]
+)
+
+@app.after_request
+def monitor_requests(response):
+    REQUEST_COUNT.labels(
+        request.method,
+        request.path,
+        response.status_code
+    ).inc()
+    return response
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login' 
